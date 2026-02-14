a6178202a48a0dd7f97c2645637558162b1ebf8a
Achyuth <achyuthrayal2@gmail.com>
2026-02-14 20:19:43 +0530

    updated state

--- state.py
+++ state.py
@@ -2,21 +2,6 @@
 from typing import TypedDict, Annotated, List, Dict, Optional
 import operator
 
-# class GraphState(TypedDict, total=False):
-#     user_id: str
-#     query: str
-#     original_query: str       
-#     final_response: str
-#     conversation_mode: Optional[str]
-#     clarification_question: Optional[str]
-#     chat_history: List[Dict]
-#     needs_assistance: bool
-#     revision_count: int
-#     rag_docs:Optional [str]
-#     error_log: List[str]
-#     web_results:Optional [List]
-#     validation_score: Optional[float]
-    
 class GraphState(TypedDict, total=False):
     user_id: str
     query: str
@@ -26,8 +11,8 @@ class GraphState(TypedDict, total=False):
 
     clarification_count: int
     force_expand: Optional[bool]
-    # NEW FIELDS
-    conversation_mode: Optional[str]  # "clarify" | "answer"
+
+    conversation_mode: Optional[str] 
     clarification_question: Optional[str]
     awaiting_clarification: bool
 
